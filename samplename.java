6ad7143e6066e9ad03e3533d8cb37a0a5695ae04
Rajini0510 <chevvururajini@gmail.com>
2025-08-27 20:29:23 +0530

    samplename.java

--- samplename.java
+++ samplename.java
@@ -1,4 +1,4 @@
-public class SampleName {
+public class samplename {
     public static void main(String[] args) {
         String name = "Rajini";       
         System.out.println("hello my name is " + name);
